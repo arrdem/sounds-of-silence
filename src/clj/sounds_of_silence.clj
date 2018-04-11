231801ba04d8166b15dd6d27f66508e99b420fac
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-04-11 13:49:41 -0700

    Add some with macros, rewrite silence and lamentation accordingly

--- src/clj/sounds_of_silence.clj
+++ src/clj/sounds_of_silence.clj
@@ -2,22 +2,48 @@
   "A clojure.test fixture"
   (:import [me.arrdem.silence NullWriter ErrorWriter]))
 
+(defmacro with-out
+  "Macro.
+
+  Executes forms in a context with `*out*` bound to the given writer."
+  [writer & forms]
+  `(binding [*out* ~writer]
+     ~@forms))
+
+(defmacro with-err
+  "Macro.
+
+  Executes forms in a context with `*err*` bound to the given writer."
+  [writer & forms]
+  `(binding [*err* ~writer]
+     ~@forms))
+
+(defmacro with-out+err
+  "Macro.
+
+  Executes the forms in a context with `*out*` and `*err*` bound to the same (given) writer."
+  [writer & forms]
+  `(let [writer# ~writer]
+     (binding [*out* writer#
+               *err* writer#]
+       ~@forms)))
+
 (defn silence
   "clojure.test fixture.
 
-  Wraps the given test fixture, running it with `*out*` and `*err*` fully suppressed."
+  Wraps the given test fixture, running it with `*out*` and `*err*` fully suppressed.
+
+  Does NOT manipulate `System/out` or `System/err`."
   [f]
-  (let [nulwrtr (NullWriter.)]
-    (binding [*out* nulwrtr
-              *err* nulwrtr]
-      (f))))
+  (with-out+err (NullWriter.)
+    (f)))
 
 (defn lamentation
   "clojure.test fixture.
 
-  Runs the given tests with `*out*` and `*err*` bound to exception producing writers."
+  Runs the given tests with `*out*` and `*err*` bound to exception producing writers.
+
+  Does NOT manipulate `System/out` or `System/err`."
   [f]
-  (let [errwtr (ErrorWriter.)]
-    (binding [*out* errwtr
-              *err* errwtr]
-      (f))))
+  (with-out+err (ErrorWriter.)
+    (f)))
